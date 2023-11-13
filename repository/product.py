449b78f03d9b9a6e6865aa9288e30c0f1409d5f9
KrzysztofPempera <k.pempera@gmail.com>
2023-11-13 23:11:06 +0100

    Add null validation to repository

--- repository/product.py
+++ repository/product.py
@@ -1,7 +1,13 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from models.product import Product
 
 def get_product_by_model(db: Session, model: str):
-    return db.query(Product).filter_by(model=model).first()
+    product = db.query(Product).filter_by(model=model).first()
+
+    if not product:
+        raise HTTPException(status_code=404, detail=f"Product with model {model} not found")
+
+    return product
 
             
