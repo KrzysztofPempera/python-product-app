e63d9c12ad50700553e5c6314e56de74a53ba649
KrzysztofPempera <k.pempera@gmail.com>
2023-11-14 16:44:04 +0100

    Add wrong type exception to calculate service

--- tests/test_calculate_service.py
+++ tests/test_calculate_service.py
@@ -27,11 +27,25 @@ def test_calculate_profitability_index_laptop_product():
     #Assert
     assert result == expected_result
 
-def test_calculate_profitability_index_with_price_zero():
+def test_calculate_profitability_index_with_price_zero_throws_exception():
     #Arrange
     product = PcProduct(ram=8, hd=512, price=0, speed=3)
     expected_exception_code = 400
     expected_exception_detail = "Product price cannot be zero"
+
+    #Act
+    with pytest.raises(HTTPException) as exc_info:
+        calculate_profitability_index(product)
+
+    #Assert
+    assert exc_info.value.status_code == expected_exception_code
+    assert exc_info.value.detail == expected_exception_detail
+
+def test_calculate_profitability_index_with_printer_product_throws_exception():
+    #Arrange
+    product = PcProduct(ram=8, hd=512, price=0, speed=3, type='printer')
+    expected_exception_code = 400
+    expected_exception_detail = "Cannot calculate profitability index of products other than pc and laptop"
 
     #Act
     with pytest.raises(HTTPException) as exc_info:
