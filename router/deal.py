6310bb0b6c69b98dfa8f71c59cb5d1213ee5d1ea
KrzysztofPempera <k.pempera@gmail.com>
2023-11-14 20:44:27 +0100

    Refactor csv creation to inmemory

--- router/deal.py
+++ router/deal.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
 from service.csv_service import create_deal_csv
 from repository.product import get_random_product_by_type
@@ -13,10 +13,12 @@ async def create_deals(db: Session = Depends(get_db)):
     laptop_product = get_random_product_by_type(db=db, type='laptop')
     printer_product = get_random_product_by_type(db=db, type='printer')
     
-    create_deal_csv(product1=pc_product, product2=printer_product)
-    create_deal_csv(product1=laptop_product, product2=printer_product)
+    csv_data = create_deal_csv(product1=pc_product, product2=printer_product)
+    csv_data = create_deal_csv(product1=laptop_product, product2=printer_product, csv_data=csv_data)
+
+    csv_data.seek(0)
 
     try:
-        return FileResponse('deal.csv', filename='deals.csv', media_type="text/csv")
+        return StreamingResponse(iter([csv_data.getvalue()]), media_type="text/csv")
     except FileNotFoundError:
-        return {"error": "File not found"}
+        return {"error": "Could not create file"}
--- service/csv_service.py
+++ service/csv_service.py
@@ -1,4 +1,5 @@
 import csv
+import io
 
 def create_index_csv(product, index):
     with open('index.csv', 'a', newline='') as csvfile:
@@ -9,23 +10,26 @@ def create_index_csv(product, index):
 
         csv_writer.writerow([product.type]+ [index])
 
-def create_deal_csv(product1, product2):
-    with open('deal.csv', 'a', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
+def create_deal_csv(product1, product2, csv_data=None):
+    if csv_data == None:
+        csv_data = io.StringIO()
+    csv_writer = csv.writer(csv_data)
 
-        if csvfile.tell() == 0:
-            csv_writer.writerow(['DEAL'] +
+    if csv_data.tell() == 0:
+        csv_writer.writerow(['DEAL'] +
                             ['FIRST PRODUCT'] +
                             [column.name.upper() for column in type(product1).__table__.columns] +
                             ['SECOND PRODUCT'] +
                             [column.name.upper() for column in type(product2).__table__.columns] +
                             ['DEAL PRICE'])
-            
-        deal_price = round((product1.price + product2.price)*0.9,2)
+        
+    deal_price = round((product1.price + product2.price)*0.9,2)
 
-        csv_writer.writerow([f'{product1.type} + {product2.type}'] +
+    csv_writer.writerow([f'{product1.type} + {product2.type}'] +
                         [''] +
                         [getattr(product1, column.name) for column in type(product1).__table__.columns] +
                         [''] +
                         [getattr(product2, column.name) for column in type(product2).__table__.columns] +
                         [deal_price])
+    
+    return csv_data
