361f993de28b2adfc999cd65edfca0c3ad9f1f8e
KrzysztofPempera <k.pempera@gmail.com>
2023-11-14 17:38:51 +0100

    Add query validation

--- router/index.py
+++ router/index.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from service.calculate_service import calculate_profitability_index
 from service.csv_service import create_index_csv
 from repository.product import get_product_by_model
 from database.connection import get_db
+from typing import Annotated
 
 router = APIRouter()
 
-@router.get("/index/{model}")
-def calculate_index(model: str, db: Session = Depends(get_db)):
+@router.get("/index/")
+def calculate_index(model: Annotated[str, Query(max_length=50)], db: Session = Depends(get_db)):
     product = get_product_by_model(db=db, model=model)
     index = calculate_profitability_index(product=product)
     create_index_csv(product=product, index=index)
